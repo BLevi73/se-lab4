143643b5c9607e19fb68a07baf2effefd8438acc
Benő Levente <benolevi@gmail.com>
2024-10-30 17:25:49 +0100

    JUnit tests for TorpedoStore class

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,20 +1,45 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TorpedoStoreTest {
+    TorpedoStore store;
+    @BeforeEach
+    void create_TorpedoStore() {
+        store = new TorpedoStore(1);
+    }
+
+    @Test
+    void torpedo_Count() {
+        assertEquals(1,store.getTorpedoCount());
+    }
 
     @Test
     void fire_Success() {
-        // Arrange
-        TorpedoStore store = new TorpedoStore(1);
-
         // Act
         boolean result = store.fire(1);
 
         // Assert
         assertEquals(true, result);
+    }
+
+    @Test
+    void correct_Torpedo_Count_After_Firing() {
+        store.fire(1);
+        assertEquals(0, store.getTorpedoCount());
+    }
+
+    @Test
+    void fire_Fail_Firing_Nothing() {
+        assertThrows(IllegalArgumentException.class,() -> store.fire(0));
+    }
+
+    @Test
+    void fire_Fail_Firing_Not_Enough_Torpedos() {
+        assertThrows(IllegalArgumentException.class,() -> store.fire(2));
     }
 }
